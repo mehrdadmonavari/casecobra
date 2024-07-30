a1c8c2419fac24237e0169a67c119d32cd871b1b
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-07-30 22:24:08 +0330

    working on design page

---
+++ app/configure/design/_components/design-configurator/resize-handle-component/index.tsx
@@ -0,0 +1,5 @@
+export const ResizeHandleComponent = () => {
+   return (
+      <div className="size-5 rounded-full shadow border border-zinc-200 bg-white transition hover:bg-primary" />
+   );
+};
