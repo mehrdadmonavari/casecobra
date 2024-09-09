54d2862c9b2fbd948b5004eaab82946713b3c5f9
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-09-09 20:39:12 +0330

    fix CORS error in production

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   async headers() {
+      return [
+         {
+            // Routes this applies to
+            source: "/api/(.*)",
+            // Headers
+            headers: [
+               // Allow for specific domains to have access or * for all
+               {
+                  key: "Access-Control-Allow-Origin",
+                  value: "*",
+                  // DOES NOT WORK
+                  // value: process.env.ALLOWED_ORIGIN,
+               },
+               {
+                  key: "Access-Control-Allow-Methods",
+                  value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+               },
+               // Allows for specific headers accepted (These are a few standard ones)
+               {
+                  key: "Access-Control-Allow-Headers",
+                  value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+               },
+            ],
+         },
+      ];
+   },
    images: {
-      domains: ["utfs.io"]
-   }
+      domains: ["utfs.io"],
+   },
 };
 
 export default nextConfig;
